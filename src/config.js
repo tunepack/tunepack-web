b95d0dbf56a9f84bf881603c0314130b1e83c493
Team Tunepack <team.tunepack@gmail.com>
2019-05-25 22:33:08 +0200

    Setup new links

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export const GA_TRACKING_ID = process.env.GA_TRACKING_ID
 
+const RELEASE_TAG = '1.0.3'
+
 export const DOWNLOAD_LINKS = {
-  mac: 'https://github.com/tunepack/tunepack-desktop/releases/download/v1.0.2/Tunepack-1.0.2.dmg',
-  win: 'https://github.com/tunepack/tunepack-desktop/releases/download/v1.0.2/Tunepack.Setup.1.0.2.exe',
-  linux: 'https://github.com/tunepack/tunepack-desktop/releases/download/v1.0.2/tunepack-desktop_1.0.2_amd64.deb'
+  mac: `https://github.com/tunepack/tunepack-desktop/releases/download/v${RELEASE_TAG}/Tunepack-${RELEASE_TAG}.dmg`,
+  win: `https://github.com/tunepack/tunepack-desktop/releases/download/v${RELEASE_TAG}/tunepack-desktop-setup-${RELEASE_TAG}.exe`,
+  linux: `https://github.com/tunepack/tunepack-desktop/releases/download/v${RELEASE_TAG}/tunepack-desktop_${RELEASE_TAG}_amd64.deb`
 }
